4c9fea6cd4829ac7cc32613b0e91212f522602f6
Kenny Westerman <ken.e.westerman@gmail.com>
2019-09-24 23:08:52 -0400

    subject_characteristics.R to output a table.

--- scripts/subject_characteristics.R
+++ scripts/subject_characteristics.R
@@ -1,84 +1,71 @@
-# Generates the numbers and group comparisons used to populate Table 1 
-# (responder and non-responder characteristics)
-
 library(readxl)
 library(readr)
 library(sqldf)
+library(dplyr)
+library(plyr)
 
-responder_yn <- read_excel("C:/Users/jfium/Dropbox/Jenny-20130612/Nutrition-Tufts/Thesis/Working/1760/Student Scholarship Application/Write Up/responder_yn.xlsx")
-X1760_wide <- read_csv("C:/Users/jfium/Dropbox/Jenny-20130612/Nutrition-Tufts/Thesis/Working/1760/Aim 2.1/Data/1760_wide_from_long.csv")
-subj_data <- merge(X1760_wide, responder_yn, by="hnrcid", all.x=T)
-sqldf('select count(*) from a where responder_yn=="No"')
-names(X1760_wide)
+K_responder_YN <- read_csv("../data/finalTbl_withPIVKAandOtherVKMeasures.csv")
+subj_data <- K_responder_YN
 
-mean(subj_data$age_0[which(subj_data$responder_yn=="Yes")])
-sd(subj_data$age_0[which(subj_data$responder_yn=="Yes")])
-mean(subj_data$age_0[which(subj_data$responder_yn=="No")])
-sd(subj_data$age_0[which(subj_data$responder_yn=="No")])
-t.test(subj_data$age_0 ~ subj_data$responder_yn)
+age <- c("Age (y)", mean(subj_data$age_0[which(subj_data$K_responder_YN=="Yes")]), 
+  sd(subj_data$age_0[which(subj_data$K_responder_YN=="Yes")]), 
+  mean(subj_data$age_0[which(subj_data$K_responder_YN=="No")]), 
+  sd(subj_data$age_0[which(subj_data$K_responder_YN=="No")]), 
+  t.test(subj_data$age_0 ~ subj_data$K_responder_YN)$p.value)
 
-table(subj_data$M1F0[which(subj_data$responder_yn=="Yes")])
-table(subj_data$M1F0[which(subj_data$responder_yn=="No")])
-chisq.test(table(subj_data$M1F0, subj_data$responder_yn))
+sex_Y <- as.data.frame(table(subj_data$M1F0[which(subj_data$K_responder_YN=="Yes")]))
+sex_N <- as.data.frame(table(subj_data$M1F0[which(subj_data$K_responder_YN=="No")]))
+sex <- c("Sex, N, (%F)", 
+         sex_Y[1,2], sex_Y[1,2]/(sex_Y[1,2]+sex_Y[2,2]), sex_N[1,2], sex_N[1,2]/(sex_N[1,2]+sex_N[2,2]),
+         chisq.test(table(subj_data$M1F0, subj_data$K_responder_YN))$p.value)
 
-mean(subj_data$bmi_0[which(subj_data$responder_yn=="Yes")])
-sd(subj_data$bmi_0[which(subj_data$responder_yn=="Yes")])
-mean(subj_data$bmi_0[which(subj_data$responder_yn=="No")])
-sd(subj_data$bmi_0[which(subj_data$responder_yn=="No")])
-t.test(subj_data$bmi_0 ~ subj_data$responder_yn)
+bmi <- c("BMI (kg/m2)", mean(subj_data$bmi_0[which(subj_data$K_responder_YN=="Yes")]), 
+         sd(subj_data$bmi_0[which(subj_data$K_responder_YN=="Yes")]), 
+         mean(subj_data$bmi_0[which(subj_data$K_responder_YN=="No")]), 
+         sd(subj_data$bmi_0[which(subj_data$K_responder_YN=="No")]), 
+         t.test(subj_data$bmi_0 ~ subj_data$K_responder_YN)$p.value)
 
-table(subj_data$smoker_mod[which(subj_data$responder_yn=="Yes")])
-table(subj_data$smoker_mod[which(subj_data$responder_yn=="No")])
-chisq.test(table(subj_data$smoker_mod, subj_data$responder_yn))
+smoker_Y <- as.data.frame(table(subj_data$smoker_0[which(subj_data$K_responder_YN=="Yes")]))
+smoker_N <- as.data.frame(table(subj_data$smoker_0[which(subj_data$K_responder_YN=="No")]))
+smoker <- c("Smoker, N, (%Y)", 
+            smoker_Y[2,2], smoker_Y[2,2]/(as.numeric(smoker_Y[2,2])+as.numeric(smoker_Y[2,1])), 
+            ifelse(is.na(smoker_N[2,2])==T, 0,smoker_N[2,2]) , 
+            ifelse(is.na(smoker_N[2,2])==T, 0,smoker_N[2,2])/(smoker_N[1,2]+(ifelse(is.na(smoker_N[2,2])==T, 0,smoker_N[2,2]))),
+         chisq.test(table(subj_data$smoker_0, subj_data$K_responder_YN))$p.value)
 
-table(subj_data$statinsyn[which(subj_data$responder_yn=="Yes")])
-table(subj_data$statinsyn[which(subj_data$responder_yn=="No")])
-chisq.test(table(subj_data$statinsyn, subj_data$responder_yn))
 
-mean(subj_data$TRIG[which(subj_data$responder_yn=="Yes")])
-sd(subj_data$TRIG[which(subj_data$responder_yn=="Yes")])
-mean(subj_data$TRIG[which(subj_data$responder_yn=="No")])
-sd(subj_data$TRIG[which(subj_data$responder_yn=="No")])
-t.test(subj_data$TRIG ~ subj_data$responder_yn)
+statins_Y <- as.data.frame(table(subj_data$statinsyn[which(subj_data$K_responder_YN=="Yes")]))
+statins_N <- as.data.frame(table(subj_data$statinsyn[which(subj_data$K_responder_YN=="No")]))
+statins <- c("Cholesterol-lowering medication use, N (%Y)", 
+             statins_Y[2,2], statins_Y[2,2]/(as.numeric(statins_Y[2,2])+as.numeric(statins_Y[2,1])), 
+            ifelse(is.na(statins_N[2,2])==T, 0,statins_N[2,2]) , 
+            ifelse(is.na(statins_N[2,2])==T, 0,statins_N[2,2])/(statins_N[1,2]+(ifelse(is.na(statins_N[2,2])==T, 0,statins_N[2,2]))),
+            chisq.test(table(subj_data$statinsyn, subj_data$K_responder_YN))$p.value)
 
-mean(subj_data$K1[which(subj_data$responder_yn=="Yes")])
-sd(subj_data$K1[which(subj_data$responder_yn=="Yes")])
-mean(subj_data$K1[which(subj_data$responder_yn=="No")])
-sd(subj_data$K1[which(subj_data$responder_yn=="No")])
-t.test(subj_data$K1 ~ subj_data$responder_yn)
+TG <- c("Baseline plasma TG (mg/dL)", mean(subj_data$TRIG[which(subj_data$K_responder_YN=="Yes")]), 
+         sd(subj_data$TRIG[which(subj_data$K_responder_YN=="Yes")]), 
+         mean(subj_data$TRIG[which(subj_data$K_responder_YN=="No")]), 
+         sd(subj_data$TRIG[which(subj_data$K_responder_YN=="No")]), 
+         t.test(subj_data$TRIG ~ subj_data$K_responder_YN)$p.value)
 
-X1760_long <- read_csv("C:/Users/jfium/Dropbox/Jenny-20130612/Nutrition-Tufts/Thesis/Working/1760/Aim 2.1/Data/1760_long.csv", 
-                       col_types = cols(CHOL_D = col_double(), HDL_D = col_double(), K1_D = col_double(), LDL_D = col_double(), 
-                                        TRIG_D = col_double(), T_OC_D = col_double(), dp_ucMGP_D = col_double(), 
-                                        p_ucOC_D = col_double(), ucOC_D = col_double()))
-X1760_long_1246 <- X1760_long[which(X1760_long$VISIT==1 | X1760_long$VISIT==2
-                                  |X1760_long$VISIT==4 | X1760_long$VISIT==6),]
+K1_b <- c("Baseline K1 (nmol/L)", mean(subj_data$baseline_K[which(subj_data$K_responder_YN=="Yes")]), 
+         sd(subj_data$baseline_K[which(subj_data$K_responder_YN=="Yes")]), 
+         mean(subj_data$baseline_K[which(subj_data$K_responder_YN=="No")]), 
+         sd(subj_data$baseline_K[which(subj_data$K_responder_YN=="No")]), 
+         t.test(subj_data$baseline_K ~ subj_data$K_responder_YN)$p.value)
 
-long_data <- as.data.frame(aggregate(X1760_long_1246[,18], list(X1760_long_1246$hnrcid), median, na.rm=T))
-names(long_data)[1] <- "hnrcid"
-names(long_data)[2] <- "K1_median"
+K1_response <- c("3-Year Change in K1 (nmol/L)", mean(subj_data$response_K[which(subj_data$K_responder_YN=="Yes")]), 
+          sd(subj_data$response_K[which(subj_data$K_responder_YN=="Yes")]), 
+          mean(subj_data$response_K[which(subj_data$K_responder_YN=="No")]), 
+          sd(subj_data$response_K[which(subj_data$K_responder_YN=="No")]), 
+          t.test(subj_data$response_K ~ subj_data$K_responder_YN)$p.value)
 
-mean(subj_data$K1[which(subj_data$responder_yn=="Yes")])
-sd(subj_data$K1[which(subj_data$responder_yn=="Yes")])
-mean(subj_data$K1[which(subj_data$responder_yn=="No")])
-sd(subj_data$K1[which(subj_data$responder_yn=="No")])
-t.test(subj_data$K1 ~ subj_data$responder_yn)
 
-subj_data <- merge(subj_data, long_data, by="hnrcid")
-mean(subj_data$K1_median[which(subj_data$responder_yn=="Yes")])
-sd(subj_data$K1_median[which(subj_data$responder_yn=="Yes")])
-mean(subj_data$K1_median[which(subj_data$responder_yn=="No")])
-sd(subj_data$K1_median[which(subj_data$responder_yn=="No")])
-t.test(subj_data$K1_median ~ subj_data$responder_yn)
-
-subj_data$change <- subj_data$K1_median - subj_data$K1
-mean(subj_data$change[which(subj_data$responder_yn=="Yes")])
-sd(subj_data$change[which(subj_data$responder_yn=="Yes")])
-mean(subj_data$change[which(subj_data$responder_yn=="No")])
-sd(subj_data$change[which(subj_data$responder_yn=="No")])
-t.test(subj_data$change ~ subj_data$responder_yn)
-
-mean(subj_data$change[which(subj_data$KorP=="1")], na.rm=T)
-sd(subj_data$change[which(subj_data$KorP=="1")], na.rm=T)
-mean(subj_data$change[which(subj_data$KorP=="0")], na.rm=T)
-sd(subj_data$change[which(subj_data$KorP=="0")], na.rm=T)
+subject_data <- as.data.frame(rbind(age, sex, bmi, smoker, statins, TG, K1_b, K1_response))
+subject_data <- plyr::rename(subject_data, c("V1"="Measure", 
+                                             "V2"="Responder Mean", "V3"="Responder SD",
+                                             "V4"="Non-Responder Mean", "V5"="Non-Responder SD", 
+                                             "V6"="p-value"))
+subject_data[,2:6] <- sapply(subject_data[,2:6], as.character)
+subject_data[,2:6] <- sapply(subject_data[,2:6], as.numeric)
+subject_data
